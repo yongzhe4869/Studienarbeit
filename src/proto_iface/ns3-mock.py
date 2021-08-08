4554c78c255cc0b9f22ddc8389338cf6291b2161
yongzhe4869 <85391069+yongzhe4869@users.noreply.github.com>
2021-08-09 00:38:34 +0200

    Update ns3-mock.py

--- src/proto_iface/ns3-mock.py
+++ src/proto_iface/ns3-mock.py
@@ -1,34 +1,70 @@
 import math
-import matplotlib.pyplot as plt
 import numpy as np
+import host 
+from env import ProtoHostEnv
+import env
+import env_pb2
 
 class data:
 
     def __init__(self):
         self.rsrp = []
-  
+        self.r = 0  
 
     def add_rsrp(self, time):
-        self.rsrp.append(time+np.sin(time+np.pi/2))
+        self.rsrp=[]
         self.rsrp.append((-time+50)+np.sin(time+np.pi/2))
+        self.rsrp.append(time+np.sin(time+np.pi/2))
         return self.rsrp
-
-    def state(self,rsrp,position):
-        self.state={'rsrp_all_BS': rsrp, 'position': position}
-        return self.state
+        # rsrp=[serving cell, neighbor cell]
     def reward(self,rsrp):
-        self.reward = max(rsrp)
-        return self.reward
-x = np.arange(50)
-rsrp_cell_1=x+np.sin(x+np.pi/2)
-rsrp_cell_2=(-x+50)+np.sin(x+np.pi/2)
-plt.plot(x,rsrp_cell_1)
-plt.plot(x,rsrp_cell_2)
-plt.legend(['cell_1','cell_2'],loc='best')
-plt.show()
+        self.r = 0.5*max(rsrp)
+        return self.r   
+    
+
+def Action(HO,rsrp):
+    # HO=1 Handover happen
+    if HO:
+        a=max(rsrp)
+        rsrp[1]= rsrp[0]
+        rsrp[0]=a
+def serving_cell(rsrp):
+    for i in range(2):
+        if rsrp[i]==max(rsrp):
+            return i
+def handover(i):
+    cell=data()
+    cell.add_rsrp(i)
+    cell_id=serving_cell(cell.rsrp)
+    HO= 0
+    if cell.rsrp[0] < max(cell.rsrp):
+        HO= 1
+        Action(HO,cell.rsrp)
+    cell.r=cell.reward(cell.rsrp)
+        
+    return cell.rsrp,cell.r, HO, cell_id
+
+        
+def main():
+    args = host.parse_args()
+    host.set_logging_config(args.logging)
+    host1 = host.ProtoHost()
+    host1.connect(args.remote, args.port)
+    init_state = env_pb2.State()
+    [init_state.value.append(x) for x in range(10)]
+    host1.send_proto(init_state)
+    for i in range(10):
+        host1.rcv_proto(env_pb2.Action)
+        s = env_pb2.EnvState()
+        state, reward, action, cell_id = handover(i)
+        s.done = i == 9
+        s.info.update(dict(iteration=str(i)))
+        s.reward = reward
+        [s.state.value.append(state[i]) for i in range(2)]
+        host1.send_proto(s)
+    
+
+if __name__ == "__main__":
+    main()
 
 
-t1=data()
-t1.add_rsrp(1)
-t1.state(t1.rsrp,[1,0,0])
-t1.reward(t1.rsrp)
