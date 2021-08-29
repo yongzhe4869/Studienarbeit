2c774ec91433d7e6bb42816a9ef3f71a3ea9e7e3
yongzhe4869 <85391069+yongzhe4869@users.noreply.github.com>
2021-08-29 17:27:05 +0200

    Update ns3-mock.py

--- src/proto_iface/ns3-mock.py
+++ src/proto_iface/ns3-mock.py
@@ -26,36 +26,18 @@ class data:
 def change(HO,rsrp):
     # HO=1 Handover happen
     if HO:
-        a=max(rsrp)
+        a=rsrp[1]
         rsrp[1]= rsrp[0]
         rsrp[0]=a    
 
-def Action(q_table):
-    action=[]
-    for k in range(11): 
-        state_action = q_table.loc[k, :]
-        #print(state_action)
-        a = np.random.choice(state_action[state_action == np.max(state_action)].index)
-        action.append(a)
-    return action
+
         
 def serving_cell(rsrp):
     for i in range(2):
         if rsrp[i]==max(rsrp):
             return i
             
-def reward(action,state):
-        if action==0:
-            if state[0]!=max(state): 
-                reward = -0.01
-            else:
-                reward = 0.5*max(state)
-        if action==1:
-            if state[0]!=max(state): 
-                reward = 0.5*max(state)
-            else:
-                reward = -0.01
-        return reward
+
        
 def handover(i):
     cell=data()
@@ -69,19 +51,6 @@ def handover(i):
     return cell.rsrp, cell_id, cell.rsrp_next
 
         
-def Agent():
-    Agent=RL.Qlearning([1,2])
-    for j in range(2):
-        for i in range(11):
-            state, cell_id, state_next=handover(i)
-            action =Agent.choose_action(i)
-            r= reward(action,state)
-            #print(state, "reward:",r ,"cell_id:",cell_id, state_next)
-            Agent.learn(i,action,r,i+1)       
-    a=Action(Agent.q_table)
-    #print(a)
-    #print(Agent.q_table)
-    return a
 
             
 def main():
@@ -89,34 +58,42 @@ def main():
     host.set_logging_config(args.logging)
     host1 = host.ProtoHost()
     host1.connect(args.remote, args.port)
+    #state, cell_id, state_next = handover(0)
     state, cell_id, state_next = handover(0)
     init_state = env_pb2.State()
     [init_state.value.append(state[x]) for x in range(2)]
     host1.send_proto(init_state)
-    for j in range(10):
+    
+    for j in range(2):
+        Action=[]
         for i in range(10):
             action=host1.rcv_proto(env_pb2.Action)
-            print(action)
+            Action.append(action.value)
+            
             s = env_pb2.EnvState()
             state, cell_id, state_next = handover(i)
             s.done = i == 9
             s.info.update(dict(iteration=str(i)))
-            if action.value == 1.0:
-                if state[0]!=max(state):
-                	s.reward = -0.01
+            #s.reward=0.5*max(state)
+            if action.value == 0:
+                if state[0]==max(state):
+                    s.reward = 0.5*max(state)
                 else:
-                	s.reward = 0.5*max(state)
-            if action.value == 2:
-                if state[0]!=max(state):
-                    	s.reward= 0.5*max(state)
+                    s.reward = -0.1
+            if action.value == 1:
+                if state[1]==max(state):
+                    s.reward= 0.5*max(state)
                 else:
-                	s.reward = -0.01
-           
-            print(s.reward)
-            change(action,state)
+                    s.reward = -0.1
+            #change(action,state)
             [s.state.value.append(state[i]) for i in range(2)]
+            print(np.array(s.state), s.reward, s.done, dict(s.info))
             host1.send_proto(s)
-	    
+        print(Action)
+        host1.send_proto(init_state)
+
+    
+    #host1.send_proto(init_state)
     
 	    
 
